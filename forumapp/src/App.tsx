3f65b5060c00ebe293152d0be101b225d5a472cc
RinatUA <feawsq@gmail.com>
2024-11-20 07:55:58 +0200

    yay react added 👾🎉🦅

---
+++ forumapp/src/App.tsx
@@ -0,0 +1,9 @@
+export function App(){
+    return (
+        <div>
+            <h1>Welcome to the Forum</h1>
+            <p>This is a platform where users can share ideas, ask questions, and collaborate.</p>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sheba.pl%2Fblog%2Fzywienie%2Fco-moze-jesc-kot-produkty-ktore-smialo-mozesz-wcielic-do-jego-diety&psig=AOvVaw29zDgMn0wvaAHnli1FvVw3&ust=1732168224742000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCM79Ob6okDFQAAAAAdAAAAABAJ" alt="Forum placeholder"/>
+        </div>
+    );
+};
